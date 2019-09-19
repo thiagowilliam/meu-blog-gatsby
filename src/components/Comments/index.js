24aa0a14582108eba7cd67cc427fd7885fc0d2fc
Thiago William Silva <thiago.will.silva@gmail.com>
2019-09-19 13:27:10 -0300

    testes discus ajuste img post

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="thiagowilliam_dev"
+        shortname="Thiago William"
         identifier={completeURL}
         title={title}
         url={completeURL}
