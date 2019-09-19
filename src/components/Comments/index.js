be621f423fa3ae81cecd45a03dcd5181fa1ff7e7
Thiago William Silva <thiago.will.silva@gmail.com>
2019-09-19 09:57:15 -0300

    alterando disqus novamente

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,13 +5,13 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `https://willianjusten.com.br${url}`
+  const completeURL = `https://thiagowilliam.com.br${url}`
 
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="disqus_a9v4tU3lD9"
+        shortname="thiagowilliam_dev"
         identifier={completeURL}
         title={title}
         url={completeURL}
